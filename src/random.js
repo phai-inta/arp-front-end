0c8c2fff8c2c539ee5dd17a3088a376696773380
Lam <lamtu1008@gmail.com>
2019-09-09 08:38:45 +1000

    Initial commit

---
+++ src/random.js
@@ -0,0 +1,33 @@
+<Row type="flex" justify="center">
+          <Col span={3}> Small</Col>
+          <Col span={10}>
+            <IntegerStep
+              min={computeEngineMin}
+              max={computeEngineMax}
+              marks={engineMarks}
+            />
+          </Col>
+          <Col span={1}>instance(s)</Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col span={3}> Medium</Col>
+          <Col span={10}>
+            <IntegerStep
+              min={computeEngineMin}
+              max={computeEngineMax}
+              marks={engineMarks}
+            />
+          </Col>
+          <Col span={1}>instance(s)</Col>
+        </Row>
+        <Row type="flex" justify="center">
+          <Col span={3}> Large</Col>
+          <Col span={10}>
+            <IntegerStep
+              min={computeEngineMin}
+              max={computeEngineMax}
+              marks={engineMarks}
+            />
+          </Col>
+          <Col span={1}>instance(s)</Col>
+        </Row>
