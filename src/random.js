1aac9b6f2bd644ad3b504e9f1b8329306560c500
Lam <lamtu1008@gmail.com>
2019-09-12 16:56:18 +1000

    Add backend call for Azure

--- src/random.js
+++ src/random.js
@@ -1,33 +1,102 @@
-<Row type="flex" justify="center">
-          <Col span={3}> Small</Col>
-          <Col span={10}>
-            <IntegerStep
-              min={computeEngineMin}
-              max={computeEngineMax}
-              marks={engineMarks}
-            />
-          </Col>
-          <Col span={1}>instance(s)</Col>
-        </Row>
-        <Row type="flex" justify="center">
-          <Col span={3}> Medium</Col>
-          <Col span={10}>
-            <IntegerStep
-              min={computeEngineMin}
-              max={computeEngineMax}
-              marks={engineMarks}
-            />
-          </Col>
-          <Col span={1}>instance(s)</Col>
-        </Row>
-        <Row type="flex" justify="center">
-          <Col span={3}> Large</Col>
-          <Col span={10}>
-            <IntegerStep
-              min={computeEngineMin}
-              max={computeEngineMax}
-              marks={engineMarks}
-            />
-          </Col>
-          <Col span={1}>instance(s)</Col>
-        </Row>
+const axios = require("axios");
+const HOURS_PER_MONTH = 730;
+
+async function getPricingForComputeEngineAzure() {
+  try {
+    const response = await axios.get(
+      "https://azure.microsoft.com/api/v3/pricing/virtual-machines/calculator/?culture=en-us&discount=mosp&v=20190910-1131-88025"
+    );
+    const smallWindows = response.data["offers"]["windows-d4v3-standard"];
+    const smallWindowsMonthly =
+      smallWindows["prices"]["perhour"]["australia-southeast"]["value"] *
+      HOURS_PER_MONTH;
+    const mediumWindows = response.data["offers"]["windows-d16v3-standard"];
+    const mediumWindowsMonthly =
+      mediumWindows["prices"]["perhour"]["australia-southeast"]["value"] *
+      HOURS_PER_MONTH;
+    const largeWindows = response.data["offers"]["windows-d48v3-standard"];
+    const largeWindowsMonthly =
+      largeWindows["prices"]["perhour"]["australia-southeast"]["value"] *
+      HOURS_PER_MONTH;
+    const smallLinux = response.data["offers"]["linux-d4v3-standard"];
+    const smallLinuxMonthly =
+      smallLinux["prices"]["perhour"]["australia-southeast"]["value"] *
+      HOURS_PER_MONTH;
+    const mediumLinux = response.data["offers"]["linux-d16v3-standard"];
+    const mediumLinuxMonthly =
+      mediumLinux["prices"]["perhour"]["australia-southeast"]["value"] *
+      HOURS_PER_MONTH;
+    const largeLinux = response.data["offers"]["linux-d48v3-standard"];
+    const largeLinuxMonthly =
+      largeLinux["prices"]["perhour"]["australia-southeast"]["value"] *
+      HOURS_PER_MONTH;
+    const computeEnginePrices = {
+      windows: {
+        small: smallWindowsMonthly,
+        medium: mediumWindowsMonthly,
+        large: largeWindowsMonthly
+      },
+      linux: {
+        small: smallLinuxMonthly,
+        medium: mediumLinuxMonthly,
+        large: largeLinuxMonthly
+      }
+    };
+    return computeEnginePrices;
+  } catch (error) {
+    console.log(
+      "Something has gone wrong while retrieving compute engine pricing for Azure"
+    );
+  }
+}
+
+async function getPricingForStorageAzure() {
+  try {
+    const response = await axios.get(
+      "https://azure.microsoft.com/api/v2/pricing/managed-disks/calculator/?culture=en-us&discount=mosp&v=20190910-1131-88025"
+    );
+    const operationPrices =
+      response.data["offers"]["transactions-ssd"]["prices"][
+        "australia-southeast"
+      ]["value"];
+    const smallDiskPrice =
+      response.data["offers"]["standardssd-e15"]["prices"][
+        "australia-southeast"
+      ]["value"];
+    const mediumDiskPrice =
+      response.data["offers"]["standardssd-e20"]["prices"][
+        "australia-southeast"
+      ]["value"];
+    const largeDiskPrice =
+      response.data["offers"]["standardssd-e30"]["prices"][
+        "australia-southeast"
+      ]["value"];
+    const storagePrice = {
+      operationPrices: operationPrices,
+      smallDiskPrice: smallDiskPrice,
+      mediumDiskPrice: mediumDiskPrice,
+      largeDiskPrice: largeDiskPrice
+    };
+    return storagePrice;
+  } catch (error) {
+    console.log(
+      "Something has gone wrong while retrieving storage pricing for Azure" +
+        error
+    );
+  }
+}
+
+async function getAzureData() {
+  const computeEnginePrices = await getPricingForComputeEngineAzure();
+  const storagePrices = await getPricingForStorageAzure();
+  const azureData = {};
+  azureData["computeEngine"] = computeEnginePrices;
+  azureData["storage"] = storagePrices;
+  return azureData;
+}
+
+var azureData = {};
+getAzureData().then(response => {
+  azureData = response;
+  console.log(azureData);
+});
